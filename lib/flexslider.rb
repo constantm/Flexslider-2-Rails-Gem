b2ef0ce633500a9cadca2ce0f0a24147cfef74fe
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-07 16:46:11 -0500

    Fixed `vendor/asset/fonts` loading
    Added `depend_on_asset` tags to SCSS, according to
    [sprockets_better_errors](https://github.com/schneems/sprockets_better_errors)

--- lib/flexslider.rb
+++ lib/flexslider.rb
@@ -3,6 +3,9 @@ require "flexslider/version"
 module Flexslider
   module Rails
     class Engine < ::Rails::Engine
+      initializer :append_dependent_assets_path, :group => :all do |app|
+        app.config.assets.paths.push(engine_root('vendor/assets/fonts'))
+      end
     end
   end
 end
