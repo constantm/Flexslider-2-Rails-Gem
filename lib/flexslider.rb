e7ec92e95ea2206e70251f235d02c4eb7ea7103e
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-07 16:51:35 -0500

    fixed engine root

--- lib/flexslider.rb
+++ lib/flexslider.rb
@@ -4,7 +4,7 @@ module Flexslider
   module Rails
     class Engine < ::Rails::Engine
       initializer :append_dependent_assets_path, :group => :all do |app|
-        app.config.assets.paths.push(engine_root('vendor/assets/fonts'))
+        app.config.assets.paths.push('vendor/assets/fonts')
       end
     end
   end
