b4ddb5512d122bdbf5d90007b8b14ee65e8b8b24
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-07 17:40:13 -0500

    added paths for precompile

--- lib/flexslider.rb
+++ lib/flexslider.rb
@@ -4,7 +4,8 @@ module Flexslider
   module Rails
     class Engine < ::Rails::Engine
       initializer :append_dependent_assets_path, :group => :all do |app|
-        app.config.assets.paths.push('vendor/assets/fonts')
+        app.config.assets.precompile += %w( flexslider.css )
+        app.config.assets.precompile += %w( fonts )
       end
     end
   end
