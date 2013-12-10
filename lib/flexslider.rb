17b7078fc7b09458625b1218ae2787affa010666
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-10 18:58:30 -0500

    fixed loading path

--- lib/flexslider.rb
+++ lib/flexslider.rb
@@ -4,8 +4,7 @@ module Flexslider
   module Rails
     class Engine < ::Rails::Engine
       initializer :append_dependent_assets_path, :group => :all do |app|
-        app.config.assets.precompile += %w( flexslider.css )
-        app.config.assets.precompile += %w( fonts )
+        app.config.assets.paths.push 'fonts'
       end
     end
   end
